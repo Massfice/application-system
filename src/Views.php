fc27b1a37c2ceda66827600804bd281b059c776d
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2020-01-26 21:34:18 +0100

    Bugfix in Views.php

--- src/Views.php
+++ src/Views.php
@@ -8,7 +8,7 @@ use Massfice\Application\ViewConfig;
 class Views {
     public static function generateView(string $template, array $data = []) {
         $loadedData = ViewConfig::assingedVariables();
-        View::getInstance(dirname(__DIR__,3)."/views",$loadedData)->generate($template, $data);
+        View::getInstance(dirname(__DIR__,4)."/views",$loadedData)->generate($template, $data);
     }
 }
 
